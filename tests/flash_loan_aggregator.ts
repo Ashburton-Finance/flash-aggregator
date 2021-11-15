9bcae73128742d628a38f5ac822aef95556f9944
MedadRufus <medadrufus@gmail.com>
2021-11-15 17:45:23 +0000

    Initial commit

---
+++ tests/flash_loan_aggregator.ts
@@ -0,0 +1,17 @@
+import * as anchor from '@project-serum/anchor';
+import { Program } from '@project-serum/anchor';
+import { FlashLoanAggregator } from '../target/types/flash_loan_aggregator';
+
+describe('flash_loan_aggregator', () => {
+
+  // Configure the client to use the local cluster.
+  anchor.setProvider(anchor.Provider.env());
+
+  const program = anchor.workspace.FlashLoanAggregator as Program<FlashLoanAggregator>;
+
+  it('Is initialized!', async () => {
+    // Add your test here.
+    const tx = await program.rpc.initialize({});
+    console.log("Your transaction signature", tx);
+  });
+});
