ea2c6553e62e1b1e8c1a179dd38efb106911ac24
MedadRufus <medad@medadnewman.co.uk>
2021-11-16 12:19:03 +0000

    put in internal tests

--- programs/flash_loan_aggregator/src/lib.rs
+++ programs/flash_loan_aggregator/src/lib.rs
@@ -68,3 +68,23 @@ pub struct FlashLoan<'info> {
     // ADD ANY ADDITIONAL ACCOUNTS THAT MAY BE EXPECTED BY THE
     // RECEIVER'S FLASHLOAN INSTRUCTION
 }
+
+fn sqrt(number: f64) -> Result<f64, String> {
+    if number >= 0.0 {
+        Ok(number.powf(0.5))
+    } else {
+        Err("negative floats don't have square roots".to_owned())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_sqrt() -> Result<(), String> {
+        let x = 4.0;
+        assert_eq!(sqrt(x)?.powf(2.0), x);
+        Ok(())
+    }
+}
