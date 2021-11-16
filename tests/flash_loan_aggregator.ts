143d08afaa71c2e9f88b21585dd7bcb1cb443fc1
MedadRufus <medad@medadnewman.co.uk>
2021-11-16 22:11:38 +0000

    lots of refactoring namespace
    successful refactor

--- tests/flash_loan_aggregator.ts
+++ tests/flash_loan_aggregator.ts
@@ -1,13 +1,13 @@
 import * as anchor from '@project-serum/anchor';
 import { Program } from '@project-serum/anchor';
-import { FlashLoanAggregator } from '../target/types/flash_loan_aggregator';
+import { Flashaggregator } from '../target/types/flashaggregator';
 
-describe('flash_loan_aggregator', () => {
+describe('flashaggregator', () => {
 
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.Provider.env());
 
-  const program = anchor.workspace.FlashLoanAggregator as Program<FlashLoanAggregator>;
+  const program = anchor.workspace.Flashaggregator as Program<Flashaggregator>;
 
   it('maxflashloan', async () => {
     // Add your test here.
