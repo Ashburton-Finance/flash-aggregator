93c708c7698e0d4bb789424c3b3fc6eef9337e7c
MedadRufus <medad@medadnewman.co.uk>
2021-11-16 15:34:47 +0000

    make a module name compilable

--- programs/flash_loan_aggregator/src/lib.rs
+++ programs/flash_loan_aggregator/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod flash_loan_aggregator {
+pub mod flashaggregator {
     use super::*;
 
     /**
