18968f9a1e666b76344e73662f7a62c3175e4fa1
MedadRufus <medad@medadnewman.co.uk>
2021-11-17 14:28:32 +0000

    fix compile test (#8)

--- programs/flashaggregator/src/lib.rs
+++ programs/flashaggregator/src/lib.rs
@@ -3,7 +3,7 @@ use anchor_lang::prelude::*;
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
 #[program]
-pub mod flashaggregator {
+pub mod flashaggregator_module {
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> ProgramResult {
