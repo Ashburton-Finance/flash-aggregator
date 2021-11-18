06e4f342d95599ec1f8ca9286fc5f8c981160c9c
MedadRufus <medad@medadnewman.co.uk>
2021-11-18 23:11:33 +0000

    put in test into our test program

--- tests/flash_loan_aggregator.ts
+++ tests/flash_loan_aggregator.ts
@@ -10,6 +10,9 @@ import {
 } from '@solana/spl-token';
 
 
+import { Blockchain } from './blockchain';
+
+
 // Need the system program, will talk about this soon.
 const { SystemProgram } = anchor.web3;
 
@@ -68,6 +71,52 @@ describe('flashaggregator', () => {
     console.log("Your transaction signature", tx);
   });
 
+  it('old @ilmoi run test', async () => {
+
+    const bc = new Blockchain();
+    await bc.getConnection();
+    await bc.initLendingMarket();
+    await bc.initReserve(bc.tokenA, 100, 40);
+    await bc.initObligation();
+    await bc.calcAndPrintMetrics();
+
+    // check user lost tokens
+    assert.equal(bc.metrics.tokenAUserBalance.value.uiAmount, 100 - 40);
+    // check protocol gained tokens
+    assert.equal(bc.metrics.tokenAProtocolBalance.value.uiAmount, 40);
+    // check user was issued LP tokens in return
+    assert.equal(bc.metrics.tokenALPUserBalance.value.uiAmount, 40);
+    // check total liquidity available
+    // @ts-ignore
+    assert.equal(bc.metrics.reserveAState.data.liquidity.availableAmount, 40n);
+
+    // --------------------------------------- depositing / withdrawing liquidity
+
+    await bc.depositReserveLiquidity(bc.tokenA, 20);
+    await bc.redeemReserveCollateral(bc.tokenA, 10);
+    await bc.calcAndPrintMetrics();
+
+    // check changes in balances add up
+    assert.equal(bc.metrics.tokenAUserBalance.value.uiAmount, 100 - 40 - 20 + 10);
+    assert.equal(bc.metrics.tokenAProtocolBalance.value.uiAmount, 40 + 20 - 10);
+
+
+    // --------------------------------------- flash loan
+
+    const oldBorrowedAmount = bc.metrics.obligState.data.borrowedValue.toNumber();
+    const oldProtocolFee = bc.metrics.tokenAProtocolFeeBalance.value.uiAmount;
+    const oldHostFee = bc.metrics.tokenAHostBalance.value.uiAmount;
+
+    await bc.borrowFlashLoan(bc.tokenA, 10);
+    await bc.calcAndPrintMetrics();
+
+    //check that fees went up, but the borrowed amount stayed the same
+    assert.equal(bc.metrics.obligState.data.borrowedValue.toNumber(), oldBorrowedAmount);
+    assert.isAbove(bc.metrics.tokenAProtocolFeeBalance.value.uiAmount, oldProtocolFee);
+    assert.isAbove(bc.metrics.tokenAHostBalance.value.uiAmount, oldHostFee);
+
+    console.log('All tests passed!');
+  });
 
 
   it('take flash loan run test', async () => {
