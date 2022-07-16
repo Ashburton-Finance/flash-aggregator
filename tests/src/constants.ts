fa33bcd31778cdc8c3685b5005a170a423dd8f00
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-07-16 20:56:03 +0100

    Put in useful comments to say we borrow from Solend

--- tests/src/constants.ts
+++ tests/src/constants.ts
@@ -1,7 +1,7 @@
 import { PublicKey } from '@solana/web3.js';
 import BigNumber from 'bignumber.js';
 
-export const LENDING_PROGRAM_ID = new PublicKey('8qdJZwaeDUPFGdbriVhhHhyNPFvE8tYjvYL7pBWS9pmM');
+export const LENDING_PROGRAM_ID = new PublicKey('8qdJZwaeDUPFGdbriVhhHhyNPFvE8tYjvYL7pBWS9pmM'); // solend devnet
 
 /** @internal */
 export const ORACLE_PROGRAM_ID = new PublicKey('gSbePebfvPy7tRqimPoVecS2UsBvYv46ynrzWocc92s');
