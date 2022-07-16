2a4371b7f9efca1d8d7cdb72f7797422b10715fd
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-07-16 02:31:29 +0100

    delete frivolous unittests

--- programs/flashaggregator/src/lib.rs
+++ programs/flashaggregator/src/lib.rs
@@ -139,23 +139,3 @@ pub struct MaxFlashLoan {}
 
 #[derive(Accounts)]
 pub struct FlashFee {}
-
-fn sqrt(number: f64) -> Result<f64, String> {
-    if number >= 0.0 {
-        Ok(number.powf(0.5))
-    } else {
-        Err("negative floats don't have square roots".to_owned())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_sqrt() -> Result<(), String> {
-        let x = 4.0;
-        assert_eq!(sqrt(x)?.powf(2.0), x);
-        Ok(())
-    }
-}
