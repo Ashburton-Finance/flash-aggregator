d386c7b6071d806d38c1f88d4f6bb98b9f4d18a0
Medad Rufus Newman <medad@medadnewman.co.uk>
2022-07-18 07:40:02 +0100

    refactor name

--- programs/flashaggregator/src/lib.rs
+++ programs/flashaggregator/src/lib.rs
@@ -31,7 +31,7 @@ pub mod flashaggregator {
      * Take a flash loan on behalf of the caller, drawing from Solend, Port Finance, starting with the cheapest flash loan fee
      * until the requested amount has been borrowed. Then pass it to the caller's account.
      */
-    pub fn flash_loan_wrapper<'info>(ctx: Context<FlashLoanWrapper>) -> ProgramResult {
+    pub fn get_flash_loan<'info>(ctx: Context<FlashLoanWrapper>) -> ProgramResult {
         // ref: https://github.com/solana-labs/solana-program-library/blob/master/token-lending/program/tests/flash_loan.rs
 
         // take a flash loan
